d8bc21ec23cd82b906956c1f3e348bcabfb1cfa4
MrPuel <theogouraud77@gmail.com>
2020-07-19 20:47:55 +0200

    add java project

---
+++ ganAdmin/core/src/main/java/fr/esgi/ganAdmin/core/Plugin.java
@@ -0,0 +1,23 @@
+package fr.esgi.ganAdmin.core;
+
+public class Plugin {
+    void start() {
+
+    }
+
+    String getMainViewName() {
+        return null;
+    }
+
+    String getName() {
+        return null;
+    }
+
+    String getDescription() {
+        return null;
+    }
+
+    void startCLI() {
+
+    }
+}
---
+++ ganAdmin/plugins/src/main/java/fr/esgi/ganAdmin/plugins/Extension.java
@@ -0,0 +1,27 @@
+package fr.esgi.ganAdmin.plugins;
+import fr.esgi.ganAdmin.core.Plugin;
+public class Extension extends Plugin {
+    public static String name = "Plugin 1";
+    public static String view = "view.fxml";
+    public static String description = "Here the description of Plugin 1";
+
+    public void start() {
+        System.out.println("Start Plugin 1");
+    }
+
+    public String getMainViewName() {
+        return view;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void startCLI() {
+        System.out.println("Start CLI of plugin 1");
+    }
+}
